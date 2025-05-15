def82bbb8ae9eaabad0e6b5600d171ea7f282b6d
Nevena Arezina <mrsnevenac@gmail.com>
2025-05-15 22:23:10 +0200

    Updated patch for assigning issues to users

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,12 @@
 import { z } from "zod"
 
 export const issueSchema = z.object({
-    title: z.string().min(1, "Title is required").max(255),
+    title: z.string().min(1, "Title is required").max(50),
     description: z.string().min(1, "Description is required"),
+});
+
+export const patchIssueSchema = z.object({
+    title: z.string().min(1).max(50).optional(),
+    description: z.string().min(1).max(2000).optional(),
+    assignedToUserId: z.string().min(1).max(255).optional().nullable()
 });
