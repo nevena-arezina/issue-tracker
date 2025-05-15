cf572419a8ac7e29b5f433aeb0d82ad812d9b81c
Nevena Arezina <mrsnevenac@gmail.com>
2025-05-15 12:24:27 +0200

    Added: delete api

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -8,7 +8,6 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
 
         <AlertDialog.Root>
             <AlertDialog.Trigger>
-
                 <Button color='red'>
                     <Pencil2Icon />
                     Delete Issue
